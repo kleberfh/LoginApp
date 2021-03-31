99f2238d6c46a70270f7295ee36740bc3c8c75fb
Kleber Fernando <canedokleberfilho@gmail.com>
2021-03-31 12:57:22 -0300

    Uploading project for review

---
+++ app/src/main/java/com/atividade/login/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.atividade.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
