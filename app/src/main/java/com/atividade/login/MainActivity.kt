87fb52d1662f9a8e6370d01a623a6e287e384d4b
Kleber Fernando <canedokleberfilho@gmail.com>
2021-03-31 18:36:44 -0300

    Clear button working & graddle update

--- app/src/main/java/com/atividade/login/MainActivity.kt
+++ app/src/main/java/com/atividade/login/MainActivity.kt
@@ -2,10 +2,21 @@ package com.atividade.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clearButton = findViewById<Button>(R.id.clear)
+        val loginInput = findViewById<EditText>(R.id.login)
+        val passwordInput = findViewById<EditText>(R.id.password)
+
+        clearButton.setOnClickListener {
+            loginInput.setText("")
+            passwordInput.setText("")
+        }
     }
 }
